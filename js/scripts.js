388da1825a4b34b271375f000a19a28aa3d53bb8
Soha Pourpirali <pourpirali.soha@gmail.com>
2019-11-16 20:15:26 -0800

    add dog button

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $("button#Meow").click(function() {
     $("ul#cat").prepend("<li>Meooooow</li>");
     $("ul#dog").prepend("<li>Baaark</li>");
+    $("ul#cat").before("<li>Cat:</li>");
+    $("ul#dog").before("<li>Dog:</li>");
   });
 
 });
